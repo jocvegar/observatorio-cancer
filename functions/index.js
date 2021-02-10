75eb06490f79f1168013894192f13e6e00984750
Jose <jocvegar@gmail.com>
2021-02-09 19:13:37 -0600

    Adds functions

---
+++ functions/index.js
@@ -0,0 +1,50 @@
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
+const gmailEmail = functions.config().gmail.login;
+const gmailPassword = functions.config().gmail.pass;
+
+admin.initializeApp();
+
+exports.sendEmail = functions.firestore
+  .document("messages/{messagesId}")
+  .onCreate(function(snapshot) {
+    const document = snapshot.data();
+    const name = document.name;
+    const email = document.email;
+    const message = document.message;
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      auth: {
+        type: "login",
+        user: gmailEmail,
+        pass: gmailPassword,
+      },
+    });
+
+    const mailOptions = {
+      from: "josevegaraquel@gmail.com",
+      to: "jocvegar@gmail.com",
+      subject: "Hit from HEKA HND",
+      text: `nombre: ${name}, email: ${email}, message: ${message}`,
+      html: `<h4>Nuevo mensaje</h4>
+            <p style="font-size: 16px;">${name}</p>
+            <p style="font-size: 12px;">${email}</p>
+            <p style="font-size: 12px;">${message}</p>
+            `,
+    };
+
+    const getDeliveryStatus = function(error, info) {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("Message sent: %s", info.messageId);
+    };
+
+    transporter.sendMail(mailOptions, getDeliveryStatus);
+  });
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
