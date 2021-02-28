1a6fc26cd451d8f59508158b41c1e9be2d27cc43
Jose <jocvegar@gmail.com>
2021-02-28 13:24:59 -0600

    fetch info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,37 @@
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      data: null,
+    };
+  },
+  methods: {
+    query: () => ({
+      query: `query {
+       patients(limit: 1) {
+        id
+        identidad
+       }
+     }`,
+    }),
+  },
+  created() {
+    axios
+      .post("https://www.registrodecancerhn.com/graphql", this.query())
+      .then((response) => {
+        console.log("data", (this.data = response.data.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
